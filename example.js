efeffef88dda16a0dc23f6fef8948088a2f2752b
Takaaki Hirano <hivedark@gmail.com>
2020-02-14 15:25:32 +0900

    generate map

--- example.js
+++ example.js
@@ -1,20 +1,23 @@
-import { Display } from "/node_modules/rot-js/lib/index.js";
+import {Display, Map, RNG, Util} from "/node_modules/rot-js/lib/index.js";
 
-let level = {
-  width: 11,
-  height: 11
-}
-let display = new Display(level);
+RNG.setSeed(2);
+
+let map = new Map.Digger();
+let display = new Display({
+  bg: 'black',
+  fg: 'white',
+  fontSize: 13
+});
 document.body.appendChild(display.getContainer());
 
-for (let i = 0; i < level.width; i++) {
-  for (let j = 0; j < level.height; j++ ) {
-    if (!i || !j || i + 1 == level.width || j + 1 == level.height) {
-      display.draw(i, j, '#', 'gray');
-    } else {
-      display.draw(i, j, '.', '#666');
-    }
-  }
+map.create(display.DEBUG);
+
+let drawDoor = function(x, y) {
+  display.draw(x, y, '+');
 }
 
-display.draw(level.width >> 1, level.height >> 1, '@', 'red');
+let rooms = map.getRooms();
+for (let i = 0; i < rooms.length; i++) {
+  let room = rooms[i];
+  room.getDoors(drawDoor);
+}
