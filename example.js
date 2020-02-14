13ba0d767d4b3336274bcea5d905f8cff64b1755
Takaaki Hirano <hivedark@gmail.com>
2020-02-14 18:47:51 +0900

    tutorial part 2

--- example.js
+++ example.js
@@ -1,62 +1,154 @@
-import { Display, Map, RNG, Util } from "./node_modules/rot-js/lib/index.js";
+import { DIRS, Display, Engine, Map, RNG, Scheduler, Util } from "./node_modules/rot-js/lib/index.js";
 
-let Game = {
-  display: null,
-  map: {},
+const mapWidth = 100;
+const mapHeight = 30;
+const numOfBoxes = 10;
 
-  init: function(container) {
-    RNG.setSeed(Math.random());
-
-    this._initDisplay(container);
-    this._generateMap();
+const Player = {
+  x: null,
+  y: null,
+  window: null,
+  keyMap: {
+    72: DIRS[8][6],
+    74: DIRS[8][4],
+    75: DIRS[8][0],
+    76: DIRS[8][2],
+    89: DIRS[8][7],
+    85: DIRS[8][1],
+    78: DIRS[8][5],
+    77: DIRS[8][3],
   },
 
-  _generateMap: function() {
-    let digger = new Map.Digger(100, 30);
-    let freeCells = [];
+  create(window) {
+    this.window = window;
 
-    let digCallback = function(x, y, contents) {
-      if (contents) {
+    return this;
+  },
+
+  place(x, y) {
+    this.x = x;
+    this.y = y;
+  },
+
+  draw(display) {
+    display.draw(this.x, this.y, '@', 'yellow');
+  },
+
+  act() {
+    Game.engine.lock();
+    this.window.addEventListener('keydown', this);
+  },
+
+  handleEvent(event) {
+    const code = event.keyCode;
+
+    if (!(code in this.keyMap)) {
+      return;
+    }
+
+    let newX = this.x + this.keyMap[code][0];
+    let newY = this.y + this.keyMap[code][1];
+    let newKey = newX + ',' + newY;
+    if (!(newKey in Game.map)) {
+      return;
+    }
+
+    Game.display.draw(this.x, this.y, Game.map[this.x + ',' + this.y]);
+    this.x = newX;
+    this.y = newY;
+    this.draw(Game.display);
+    this.window.removeEventListener('keydown', this);
+    Game.engine.unlock();
+  },
+}
+
+const Game = {
+  display: null,
+  map: {},
+  player: null,
+  engine: null,
+
+  init(container, window) {
+    RNG.setSeed(Math.random());
+
+    this._initPlayer(window);
+    this._initDisplay(container);
+    this._generateMap();
+    this._initEngine();
+  },
+
+  _initEngine() {
+    const scheduler = new Scheduler.Simple();
+    scheduler.add(this.player, true);
+
+    this.engine = new Engine(scheduler);
+    this.engine.start();
+  },
+
+  _initPlayer(window) {
+    this.player = Player.create(window);
+  },
+
+  _initDisplay(container) {
+    this.display = new Display({
+      bg: 'black',
+      fg: 'white',
+      width: mapWidth,
+      height: mapHeight,
+      fontSize: 16
+    });
+    container.appendChild(this.display.getContainer());
+  },
+
+  _generateMap() {
+    const digger = new Map.Digger(mapWidth, mapHeight, {
+      roomWidth: [3, 15],
+      roomHeight: [3, 9],
+      dugPercentage: [0.3],
+    });
+    const freeCells = [];
+
+    digger.create((x, y, contents) => {
+      if (contents === 1) {
         return;
       }
 
-      let key = x + ',' + y;
+      const key = `${x},${y}`;
       this.map[key] = '.';
       freeCells.push(key);
-    }
-    digger.create(digCallback.bind(this));
+    });
 
     this._generateBoxes(freeCells);
     this._drawWholeMap();
+    this._createPlayer(freeCells);
   },
 
-  _generateBoxes: function(freeCells) {
-    for (let i = 0; i < 10; i++) {
-      let index = Math.floor(RNG.getUniform() * freeCells.length);
-      let key = freeCells.splice(index, 1)[0];
+  _generateBoxes(freeCells) {
+    for (let i = 0; i < numOfBoxes; i++) {
+      const index = Math.floor(RNG.getUniform() * freeCells.length);
+      const key = freeCells.splice(index, 1)[0];
       this.map[key] = '*';
     }
   },
 
-  _drawWholeMap: function() {
-    for (var key in this.map) {
-      let parts = key.split(',');
-      let x = parseInt(parts[0]);
-      var y = parseInt(parts[1]);
+  _createPlayer(freeCells) {
+    const index = Math.floor(RNG.getUniform() * freeCells.length);
+    const key = freeCells.splice(index, 1)[0];
+    const parts = key.split(',');
+    const x = parseInt(parts[0]);
+    const y = parseInt(parts[1]);
+    this.player.place(x, y);
+    this.player.draw(this.display);
+  },
+
+  _drawWholeMap() {
+    for (let key in this.map) {
+      const parts = key.split(',');
+      const x = parseInt(parts[0]);
+      const y = parseInt(parts[1]);
       this.display.draw(x, y, this.map[key]);
     }
   },
-
-  _initDisplay: function(container) {
-    this.display = new Display({
-      bg: 'black',
-      fg: 'white',
-      width: 100,
-      height: 30,
-      fontSize: 16
-    });
-    container.appendChild(this.display.getContainer());
-  }
 }
 
 export { Game as default }
